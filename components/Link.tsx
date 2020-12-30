b75d569c2640f82cd2dac123e8555afa0ab6f1d9
Bibek <ghim.bibek@gmail.com>
2020-12-29 21:15:25 -0800

    Fix type error in Link.tsx

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,24 +1,24 @@
-import * as React from "react";
-import isPropValid from "@emotion/is-prop-valid";
-import styled from "@emotion/styled";
-import { space, SpaceProps, layout, LayoutProps } from "styled-system";
-import NextLink from "next/link";
-import * as buttonlikeStyles from "styles/components/buttonlike";
-import theme from "styles/theme";
+import * as React from "react"
+import isPropValid from "@emotion/is-prop-valid"
+import styled from "@emotion/styled"
+import { space, SpaceProps, layout, LayoutProps } from "styled-system"
+import NextLink from "next/link"
+import * as buttonlikeStyles from "styles/components/buttonlike"
+import theme from "styles/theme"
 
 const linkBaseStyles = (props: LinkStyleProps) => ({
   textDecoration: props.underline ? "underline" : "none",
   color: props.color ?? theme.colors.linkBlue,
-});
+})
 
 interface LinkStyleProps extends SpaceProps, LayoutProps {
-  asButton?: boolean;
+  asButton?: boolean
   // button variants are only used if asButton is true
-  buttonStyle?: keyof typeof buttonlikeStyles.styleVariants;
-  buttonSize?: keyof typeof buttonlikeStyles.sizeVariants;
-  underline?: boolean;
-  color?: string;
-  overrideStyles?: any;
+  buttonStyle?: keyof typeof buttonlikeStyles.styleVariants
+  buttonSize?: keyof typeof buttonlikeStyles.sizeVariants
+  underline?: boolean
+  color?: string
+  overrideStyles?: any
 }
 
 // We sometimes want to style Links identically to the way we style
@@ -37,40 +37,50 @@ const args = [
       ? {
           ...buttonlikeStyles.baseButtonStyles,
           // Mimic button variants with a "button-" prefix
-          ...(props.buttonStyle ? buttonlikeStyles.styleVariants[props.buttonStyle] : {}),
-          ...(props.buttonSize ? buttonlikeStyles.sizeVariants[props.buttonSize] : {}),
+          ...(props.buttonStyle
+            ? buttonlikeStyles.styleVariants[props.buttonStyle]
+            : {}),
+          ...(props.buttonSize
+            ? buttonlikeStyles.sizeVariants[props.buttonSize]
+            : {}),
           textDecoration: "none",
         }
       : linkBaseStyles(props),
   (props: LinkStyleProps) => props.overrideStyles,
-] as const;
+] as const
 
-interface InternalLinkProps extends React.ComponentProps<typeof NextLink>, LinkStyleProps {}
+interface InternalLinkProps
+  extends React.ComponentProps<typeof NextLink>,
+    LinkStyleProps {}
 
 const ExternalLink = styled("a", {
   shouldForwardProp: (prop) => isPropValid(prop) && prop !== "underline",
-})(...args);
+})(...args)
 const InternalLink = styled(NextLink, {
   shouldForwardProp: (prop) => isPropValid(prop) && prop !== "underline",
-})(...args);
+})(...args)
 
 const defaultProps = {
   asButton: false,
   buttonStyle: "primary",
   buttonSize: "normal",
-} as const;
-ExternalLink.defaultProps = defaultProps;
-InternalLink.defaultProps = defaultProps;
+} as const
+ExternalLink.defaultProps = defaultProps
+InternalLink.defaultProps = defaultProps
 
 interface UnifiedLinkProps extends InternalLinkProps, LinkStyleProps {
-  href: string;
-  type?: "internal" | "external";
-  ref?: any;
+  href: string
+  type?: "internal" | "external"
+  ref?: any
+  as?: any
 }
 
 const isExternalLink = (url: string) => {
-  return typeof url === "string" && (url.startsWith("http") || url.startsWith("mailto"));
-};
+  return (
+    typeof url === "string" &&
+    (url.startsWith("http") || url.startsWith("mailto"))
+  )
+}
 
 // An isomorphic Link component where the link is always passed in via the "to" prop so that you
 // don't have to decide whether to pass in "to" for the react-router InternalLink or "href" for
@@ -81,8 +91,8 @@ const UnifiedLink = ({ href, ref, ...props }: UnifiedLinkProps) => {
     <ExternalLink ref={ref} {...props} href={href} />
   ) : (
     <InternalLink {...props} href={href} />
-  );
-};
+  )
+}
 
-export { ExternalLink, InternalLink, UnifiedLink as Link };
-export default UnifiedLink;
+export { ExternalLink, InternalLink, UnifiedLink as Link }
+export default UnifiedLink
